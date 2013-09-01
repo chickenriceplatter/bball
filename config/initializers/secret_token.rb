ba79e17e4d70b6d81189511adf1a2026bbbfa0fa
chickenriceplatter <chickenriceplatter@gmail.com>
2013-09-01 00:55:50 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bball::Application.config.secret_key_base = '3be9adde6865a17b619ef6e1b79726653767b3574f62908db3a7590219c15ee56f903d58006c0c35652e6ec4cc2dcd480d75e53cc7496d3cba51609b45ca5da2'
